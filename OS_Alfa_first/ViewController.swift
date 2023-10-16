49bfa6e1f3420530bd48fdc209a1573ae847d608
Melli <mailto:nspershina@edu.hse.ru>
2023-10-16 19:42:20 +0300

    added url session

--- OS_Alfa_first/ViewController.swift
+++ OS_Alfa_first/ViewController.swift
@@ -12,8 +12,21 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = . systemPink
+        let url: URL = URL(string: "https://api.punkapi.com/v2/beers")! // принудительная распаковка используется только в тестах
+        URLSession.shared.dataTask(with: url, completionHandler: { data, response, error in
+            guard 
+                let data = data,
+                let response,
+                error == nil
+            else{
+                return
+            }
+         //   let decoder = JSONDecoder()
+            // Обработка данных
+            let str = String(data: data, encoding: .utf8)
+            print("Полученные данные : \(str ?? "")")
+        }).resume()
+        // клажуры (лямбда функци, completionHandler) - сохранение функций в переменных и передача функции в качестве аргумента. Нужно для того, чтобы не тормозить код, который будет выполняься после отправки запроса на сервер. Он принимает 3 аргумента из самой системы, ничего не возвращает. // с 16 строки
     }
-
-
 }
 
